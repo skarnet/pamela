c2d4dfaa55d336c982b71d79411386ca4da126d1
Laurent Bercot <ska-skaware@skarnet.org>
2021-09-02 17:14:51 +0000

    Pass errno as unsigned char
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/pamela/pamela_query_string.c
+++ src/pamela/pamela_query_string.c
@@ -14,7 +14,7 @@ int pamela_query_string (pamela_t *a, char const *s, size_t len, stralloc *sa)
   if (textmessage_timed_receive(&a->in, &v, 0, 0) <= 0) return 0 ;
   if (!v.iov_len) return (errno = EPROTO, 0) ;
   s = v.iov_base ;
-  if (s[0]) return (errno = s[0], 0) ;
+  if (s[0]) return (errno = (unsigned char)s[0], 0) ;
   if (!stralloc_catb(sa, s + 1, v.iov_len - 1)) return 0 ;
   return 1 ;
 }
